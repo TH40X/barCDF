5daf5ecd0c48f181fa32904fe8b1709873011ed4
Theo Manfredi <theo.manfredi@grenoble-inp.fr>
2022-07-17 16:48:40 +0200

    suppr password

--- main.py
+++ main.py
@@ -32,10 +32,7 @@ def render(name):
 
 @app.route("/")
 def index():
-    if "username" in session:
-        return render_template('main.html', userList=userList)
-    else:
-        return render_template('login.html')
+    return render_template('main.html', userList=userList)
 
 
 @app.route("/login", methods=['POST'])
@@ -52,74 +49,56 @@ def manage():
     if time.time() - initTime > 60:
         saveUserList()
         initTime = time.time()
-    if "username" in session:
-        num = request.args.get("num")
-        user = userList[int(num)]
-        return render_template('manage.html', user=user)
-    else:
-        return render_template('login.html')
+    num = request.args.get("num")
+    user = userList[int(num)]
+    return render_template('manage.html', user=user)
 
 
 @app.route("/encaisser", methods=["GET"])
 def encaisser():
-    if "username" in session:
-        num = request.args.get("num")
-        user = userList[int(num)]
-        return render_template('encaisser.html', user=user)
-    else:
-        return render_template('login.html')
+    num = request.args.get("num")
+    user = userList[int(num)]
+    return render_template('encaisser.html', user=user)
 
 
 @app.route("/encaisserValid", methods=["POST"])
 def encaisserValid():
-    if "username" in session:
-        num = request.form.get("num")
-        value = request.form.get("montant")
-        user = userList[int(num)]
-        try:
-            user.sold += int(value)
-        except:
-            pass
-        return render_template('main.html', userList=userList)
-    else:
-        return render_template('login.html')
+    num = request.form.get("num")
+    value = request.form.get("montant")
+    user = userList[int(num)]
+    try:
+        user.sold += int(value)
+    except:
+        pass
+    return render_template('main.html', userList=userList)
 
 
 @app.route("/vente", methods=["GET"])
 def vente():
-    if "username" in session:
-        num = request.args.get("num")
-        user = userList[int(num)]
-        return render_template('vente.html', user=user)
-    else:
-        return render_template('login.html')
+    num = request.args.get("num")
+    user = userList[int(num)]
+    return render_template('vente.html', user=user)
 
 @app.route("/historique", methods=["GET"])
 def historique():
-    if "username" in session:
-        num = request.args.get("num")
-        user = userList[int(num)]
-        logs = user.log.split("#")
-        return render_template('historique.html', user=user, logs=logs)
-    else:
-        return render_template('login.html')
+    num = request.args.get("num")
+    user = userList[int(num)]
+    logs = user.log.split("#")
+    return render_template('historique.html', user=user, logs=logs)
 
 
 @app.route("/venteValid", methods=["POST"])
 def venteValid():
-    if "username" in session:
-        num = request.form.get("num")
-        user = userList[int(num)]
-        total = 0
-        for item in bar:
-            amount = int("0" + request.form.get(item))
-            if amount > 0:
-                user.log += "# {} x {}".format(item, amount)
-                total += bar[item] * amount
-        user.sold -= total
-        return render_template('main.html', userList=userList)
-    else:
-        return render_template('login.html')
+    num = request.form.get("num")
+    user = userList[int(num)]
+    total = 0
+    for item in bar:
+        amount = int("0" + request.form.get(item))
+        if amount > 0:
+            user.log += "# {} x {}".format(item, amount)
+            total += bar[item] * amount
+    user.sold -= total
+    return render_template('main.html', userList=userList)
 
 def saveUserList():
     with open("save.txt" + str(int(time.time())), "w") as file:
@@ -132,18 +111,12 @@ def saveUserList():
 
 @app.route("/inscription", methods=["GET"])
 def inscription():
-    if "username" in session:
-        return render_template('inscription.html')
-    else:
-        return render_template('login.html')
+    return render_template('inscription.html')
 
 @app.route("/newUser", methods=["POST"])
 def newUser():
-    if "username" in session:
-        nom = request.form.get("nom")
-        tel = request.form.get("tel")
-        newId = len(userList) + 1
-        userList[newId] = User(nom, newId, tel)
-        return render_template('confirmUser.html', id=newId)
-    else:
-        return render_template('login.html')
+    nom = request.form.get("nom")
+    tel = request.form.get("tel")
+    newId = len(userList) + 1
+    userList[newId] = User(nom, newId, tel)
+    return render_template('confirmUser.html', id=newId)
